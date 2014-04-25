a4537bae35eaaeb67a321a9e2b9f9796e0a7aad6
Simon Broggi <simon.broggi@gmail.com>
2014-04-25 15:50:38 +0200

    initial commit

---
+++ Assets/HttpProcessor.cs
@@ -0,0 +1,181 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.IO;
+
+using System.Net;
+using System.Net.Sockets;
+
+using System.Text;
+
+public class HttpProcessor {
+
+	public TcpClient socket;
+	public UnityServer srv;
+
+	public NetworkStream netStream;
+
+	public string http_method;
+	public string http_url;
+	public string http_protocol_versionstring;
+	public Hashtable httpHeadedrs = new Hashtable();
+
+	private string error = null;
+
+	public HttpProcessor(TcpClient s, UnityServer srv){
+		this.socket = s;
+		this.srv = srv;
+	}
+
+	public IEnumerator process(){
+		netStream = socket.GetStream();
+
+		// wait until request is parsed
+		yield return srv.StartCoroutine(parseRequest());
+
+		if(error == null){
+
+			// wait until headers are parsed
+			yield return srv.StartCoroutine(parseHeaders());
+
+			if(error == null){
+
+				switch(http_method){
+				case "GET":
+					yield return srv.StartCoroutine(handleGetRequest());
+					break;
+				case "POST":
+					yield return srv.StartCoroutine(handlePostRequest());
+					Debug.LogWarning("todo: controll stuff in unity");
+					break;
+				default:
+					Debug.LogError("Unknown http_method " + http_method);
+					error = "Unknown http_method " + http_method;
+					break;
+				}
+			}
+		}
+
+		socket.Close();
+		netStream.Close();
+	}
+
+	private IEnumerator parseRequest(){
+
+		int startFrame = Time.frameCount;
+
+
+		//todo: fix this fubar code. try netStream.Read as in post
+		while(!netStream.DataAvailable){
+
+			Debug.LogError("No data in request");
+			error = "no data in request";
+			yield break;
+
+			//todo: wait a bit until timeout
+			//yield return new WaitForSeconds(0.1f);
+			//Debug.LogWarning("no request data. todo: timeout?");
+		}
+
+		// read a line from NetworkStream
+		string request = null;
+		yield return srv.StartCoroutine(readLine(netStream, value => request = value)); // danger??   http://answers.unity3d.com/questions/207733/can-coroutines-return-a-value.html
+		//Debug.Log (" got request string: "+request);
+		string[] tokens = request.Split(' ');
+		if(tokens.Length != 3){
+			Debug.LogError("Invalid http request line");
+			error = "Invalid http request line";
+			yield break;
+		}
+		http_method = tokens[0].ToUpper();
+		http_url = tokens[1]; //todo: add index.html if it's a folder?
+		http_protocol_versionstring = tokens[2];
+	}
+
+	private IEnumerator parseHeaders(){
+		string line = null;
+		//read lines until ""
+		while(line != ""){
+			yield return srv.StartCoroutine(readLine(netStream, value => line=value));
+			if(line.Equals("")){
+				//Debug.Log("Got headers");
+			}
+			else{
+				int separator = line.IndexOf(':');
+				if(separator == -1){
+					Debug.LogError("invalid http header line: "+line);
+					error = "invalid http header line: "+line;
+					//todo: how to handle failure?
+				}
+				else{
+					string name = line.Substring(0, separator);
+					int pos = separator+1;
+					while((pos < line.Length) && (line[pos]==' ')){
+						pos++; //strip spaces
+					}
+					string value = line.Substring(pos, line.Length - pos);
+					httpHeadedrs[name] = value;
+				}
+			}
+		}
+	}
+	private IEnumerator handleGetRequest(){
+		return srv.handleGetRequest(this);
+	}
+	private IEnumerator handlePostRequest(){
+		byte[] readBuffer = new byte[1024];
+		int bytesRead = netStream.Read(readBuffer, 0, readBuffer.Length);
+		if(netStream.DataAvailable){
+			Debug.LogWarning("didn't read all data");
+		}
+
+		return srv.handlePostRequest(this, readBuffer);
+	}
+
+	private IEnumerator readLine(NetworkStream stream, Action<string> line){
+		StringBuilder currentLine = new StringBuilder("");
+		int i;
+		char c;
+		bool lineRead = false;
+		string finalLine = null;
+		while(!lineRead){
+			i = stream.ReadByte();
+			if(i >= 0){
+				c = (char)i;
+				if(c == '\r' || c == '\n'){
+					finalLine = currentLine.ToString();
+					lineRead = true;
+				}
+				else{
+					currentLine.Append(c);
+				}
+			}
+			else{
+
+				//yield return new WaitForSeconds(0.1f);
+				//todo: better yield
+				finalLine = currentLine.ToString();
+				lineRead = true;
+				Debug.LogWarning("no full line to read. todo: timeout?");
+			}
+		}
+		line(finalLine);//pass retrieved result
+		yield break;
+	}
+	public void writeSuccess(string content_type="text/html") {
+		writeLine("HTTP/1.0 200 OK");            
+		writeLine("Content-Type: " + content_type);
+		writeLine("Connection: close");
+		writeLine("");
+	}
+	public void writeFailure() {
+		writeLine("HTTP/1.0 404 File not found");
+		writeLine("Connection: close");
+		writeLine("");
+	}
+	public void writeLine(string s){
+		byte[] b = Encoding.UTF8.GetBytes(s+"\n");
+		netStream.Write(b, 0, b.Length);
+	}
+}
---
+++ Assets/UnityServer.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Text;
+
+public class UnityServer : MonoBehaviour {
+
+	public int port = 8080;
+	public IPAddress listen_ip = Dns.GetHostEntry("localhost").AddressList[0];
+
+	//public float ListenDelay = 0.1f;
+	private WaitForEndOfFrame wait = new WaitForEndOfFrame();
+
+	private string local_public_html_url;
+
+	void Start () {
+		local_public_html_url = Application.dataPath;
+		if(Application.isEditor){
+			local_public_html_url += "/../public_html/";
+		}
+		else{
+			local_public_html_url += "/public_html/";
+		}
+		local_public_html_url = Path.GetFullPath(local_public_html_url);
+		local_public_html_url = local_public_html_url.Substring(0, local_public_html_url.Length-1);//remove last /
+		local_public_html_url = "file://"+local_public_html_url;
+
+		Debug.Log("Starting server. html files should be at "+local_public_html_url);
+
+		StartCoroutine(listen(listen_ip, port));
+	}
+
+	// rotate just to check if it hangs...
+	public float speed = 50f; 
+	void Update () {
+		transform.Rotate(Vector3.up*Time.deltaTime*speed);
+	}
+
+	private IEnumerator listen(IPAddress listen_ip, int port){
+		TcpListener listener = new TcpListener( listen_ip, port );
+		listener.Start();
+		while(gameObject.activeSelf){
+
+			if(listener.Pending()){
+				TcpClient s = listener.AcceptTcpClient();
+				//Debug.Log("got a client! create a HttpProcessor for him and start the process coroutine");
+				HttpProcessor processor = new HttpProcessor(s, this);
+				StartCoroutine(processor.process());
+			}
+
+			yield return wait;
+			//yield return new WaitForSeconds(ListenDelay);
+		}
+	}
+
+	public IEnumerator handleGetRequest(HttpProcessor p){
+		Debug.Log("Handling request: "+p.http_url);
+
+		string localFileUrl = p.http_url;
+		if(localFileUrl.EndsWith("/")){
+			localFileUrl = localFileUrl + "index.html";
+		}
+		localFileUrl = local_public_html_url + localFileUrl;
+
+		WWW www = new WWW(localFileUrl);
+
+		yield return www;
+		if(!String.IsNullOrEmpty(www.error)){
+			Debug.LogError(www.error);
+			p.writeFailure();
+		}
+		else{
+			if(p.http_url.EndsWith(".ico")){
+				p.writeSuccess(content_type:"image/x-icon");
+			}
+			else if(p.http_url.EndsWith(".png")){
+				p.writeSuccess(content_type:"image/png");
+			}
+			else if(p.http_url.EndsWith(".jpg") || p.http_url.EndsWith("jpeg")){
+				p.writeSuccess(content_type:"image/jpeg");
+			}
+			else if(p.http_url.EndsWith(".js")){
+				p.writeSuccess(content_type:"application/x-javascript");
+			}
+			else{
+				p.writeSuccess();
+			}
+			p.netStream.Write(www.bytes, 0, www.bytes.Length);
+		}
+		//p.writeLine("<html><body><h1>unity web server</h1></br><img src=\"bear.png\" alt=\"Bear\">");
+		yield break;
+	}
+	public IEnumerator handlePostRequest(HttpProcessor p, byte[] data){
+		Debug.Log("Handling POST: "+p.http_url);
+		Debug.Log(Encoding.UTF8.GetString(data));
+		p.writeSuccess();
+		p.writeLine("<h1>unity web server posted</h1></br>");
+		yield break;
+	}
+}
